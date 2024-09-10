f336c8736acb07678fd8c985907cc7a6085c011a
vhemanthsai68 <vhemanthsai68@gmail.com>
2024-09-11 00:11:44 +0530

    Overriding files with new versions

--- Client.java
+++ Client.java
@@ -1,36 +1,63 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.Scanner;
 
+import java.io.IOException;
+
 public class Client{
+	private static final int PORT = 16000; //Enter the port number of server process here
+	private static final String serverAddress = "localhost"; //Enter server ip address here
+	private static Socket cs = null;
+	private static Scanner sc = new Scanner(System.in);
+	private static BufferedReader br = null;
+	private static PrintWriter pw = null;
+	private static String smsg = "";
+	private static String cmsg = "";
+	private static int choice = 0;
 	public static void main(String[] args)throws IOException{
-		String serverAddress = "localhost";
-		int port = 16000;
-		PrintWriter pw = null;
-		BufferedReader br = null;
-		Socket cs=null;
-		Scanner sc = new Scanner(System.in);
+		connectToServer();
+		chatWithServer();
+	}
+	public static void connectToServer()throws IOException{
+		System.out.println("Connecting to server...");
+		cs = new Socket(serverAddress, PORT);
+		System.out.println("Connected to server whose ip address: "+cs.getInetAddress()+" and port no.: "+cs.getPort()+" successfully");
+	}
+	public static void chatWithServer()throws IOException{
 		try{
+			br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+			pw = new PrintWriter(cs.getOutputStream(), true);
+
 			while(true){
 				System.out.print("Enter message: ");
-				String msg = sc.nextLine();
-				cs = new Socket(serverAddress, port);
-				System.out.println("Connected to server");
-				br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-				pw = new PrintWriter(cs.getOutputStream(), true);
-				pw.println(msg);
-				String smsg = br.readLine();
-				if(smsg != null) System.out.println("Message from server: "+smsg);
-				System.out.print("Enter 1 to end connection(0 - continue): ");
-				int i = sc.nextInt();
-				if(i == 1){
-					cs = null;
-					System.out.println("Disconnected from server");
-					return;
-				}	
+				cmsg = sc.nextLine();
+				pw.println(cmsg);
+				System.out.println("Response sent");
+
+				System.out.println("Waiting for response...");
+				smsg = br.readLine();
+				if(smsg != null) System.out.println("Message from client: "+smsg);
+				
+				System.out.print("Enter 0 to end connection (1 - continue): ");
+				choice = sc.nextInt();
+				sc.nextLine();
+				if(choice == 0){
+					pw.println("END"); //To end connection
+					System.out.println("Disconnected from server successfully");
+					break;
+				}
 			}
+		}catch(SocketException | EOFException e){
+			System.out.println("Server disconnected or connection lost");
 		}catch(Exception e){
 			e.printStackTrace();
+		}finally{
+			sc.close();
+			if(cs != null) cs.close();
 		}
 	}
 }
--- Server.java
+++ Server.java
@@ -1,39 +1,72 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
+import java.io.IOException;
+
 public class Server{
+	private static final int PORT = 16000; //Port number
+	private static ServerSocket ss = null;
+	private static Socket cs = null;
+	private static Scanner sc = null;
+	private static BufferedReader br = null;
+	private static PrintWriter pw = null;
+	private static String cmsg = "";
+	private static String smsg = "";
+	private static int choice = 0;
 	public static void main(String[] args)throws IOException{
-		ServerSocket ss=null;
-		Socket cs=null;
-		Scanner sc = new Scanner(System.in);
+		initScanner();
+		initServer();
+		startServer();
+		handleClient();
+	}
+	public static void initScanner(){
+		sc = new Scanner(System.in);
+	}
+	public static void initServer()throws IOException{
+		ss = new ServerSocket(PORT);
+	}
+	public static void startServer()throws IOException{
+		System.out.println("Server is running at: "+ss.getLocalSocketAddress()+" and listening at TCP port no. : "+ss.getLocalPort()+"...");
+		System.out.println("Waiting for a client...");
+		cs = ss.accept();
+		System.out.println("I got a client with IPv4 address: "+cs.getRemoteSocketAddress()+" and port no. : "+cs.getPort());
+	}
+	public static void handleClient()throws IOException{
 		try{
-			ss = new ServerSocket(16000);
+			br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+			pw = new PrintWriter(cs.getOutputStream(), true);
+
 			while(true){
-				System.out.println("Server is running and listening at TCP port no. 16000...");
-				System.out.println("Waiting for a client...");
-				cs = ss.accept();
-				System.out.println("I got a client");
-				BufferedReader br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-				PrintWriter pw = new PrintWriter(cs.getOutputStream(), true);
-				String msg = br.readLine();
-				if(msg != null) System.out.println("Message from client: "+msg);
+				System.out.println("Waiting for response...");
+				cmsg = br.readLine();
+				if(cmsg.equals("END")){
+					System.out.println("Client got disconnected");	
+					break;
+				}
+				if(cmsg != null) System.out.println("Message from client: "+cmsg);
 				System.out.print("Enter message: ");
-				String smsg = sc.nextLine();
+				smsg = sc.nextLine();
 				pw.println(smsg);
 				System.out.println("Response sent");
-				int i=0;
-				if(cs == null){
-					System.out.println("Client got disconnected");	
-					System.out.println("Server is going to shutdown...");	
-					ss.close();
+				System.out.print("Enter 0 to stop server (1 - continue): ");
+				choice = sc.nextInt();
+				sc.nextLine();
+				if(choice == 0){
+					System.out.println("Server is going to shutdown...");
+					System.out.println("Server is down");
+					break;
 				}
-				System.out.print("Enter 1 to stop server(0 - continue): ");
-				i = sc.nextInt();
-				if(i == 1) ss.close();
 			}
 		}catch(Exception e){
 			e.printStackTrace();
+		}finally{
+			sc.close();
+			if(cs != null) cs.close();
+			if(ss != null) ss.close();
 		}
 	}
 }
